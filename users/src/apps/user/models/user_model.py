a9348eb5132bf6f242a6bb7f093d37d808f8d6b0
michal999 <71217112+mmyszak999@users.noreply.github.com>
2022-08-18 11:44:46 +0200

    fix database migrations and create user response

---
+++ users/src/apps/user/models/user_model.py
@@ -0,0 +1,19 @@
+from uuid import UUID
+from sqlalchemy import Boolean, Column, Integer, String, Date
+
+from users.src.database import Base
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    first_name = Column(String(length=50), nullable=False)
+    last_name = Column(String(length=75), nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    username = Column(String(length=50), nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    birth_date = Column(String, nullable=False)
+    is_active = Column(Boolean, nullable=False, server_default="true")
+
+
---
+++ users/src/apps/user/schemas/user_schema.py
@@ -0,0 +1,42 @@
+from uuid import UUID
+from pydantic import BaseModel, Field
+
+
+class UserLoginInputSchema(BaseModel):
+    username: str
+    password: str
+
+
+class UserBaseSchema(BaseModel):
+    first_name : str = Field(max_length=50)
+    last_name : str = Field(max_length=75)
+    email : str = Field(max_length=50)
+    birth_date : str = Field(max_length=50)
+    username : str = Field(max_length=50)
+    
+    class Config:
+        orm_mode = True
+
+
+class UserRegisterSchema(UserBaseSchema):
+    password: str = Field(min_length=8, max_length=40)
+    password_repeat: str = Field(min_length=8, max_length=40)
+
+    class Config:
+        orm_mode = False
+
+
+class UserInputSchema(UserBaseSchema):
+    pass
+
+    class Config:
+        orm_mode = True
+
+
+class UserOutputSchema(UserBaseSchema):
+    id: int
+    is_active: bool = True
+
+    class Config:
+        orm_mode = True
+     
