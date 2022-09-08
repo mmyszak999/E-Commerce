a6f730e4df3917dabfa2232151d89e0fe3f94a6d
michal999 <71217112+mmyszak999@users.noreply.github.com>
2022-09-08 15:54:51 +0200

    modify test database fixtures

--- tests/test_users/fixtures.py
+++ tests/test_users/fixtures.py
@@ -1,24 +1,60 @@
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.engine import Engine
+from sqlalchemy.event import listens_for
+from httpx import Client
 
 from src.settings.db_settings import settings
 from src.apps.user.utils import get_db
+from src.apps.user.database import Base
 from main import app
 
 
-
-DATABASE_URL = 'postgresql://{}:{}@{}:{}/test'.format(
+@pytest.fixture()
+def db_engine():
+    DATABASE_URL = 'postgresql://{}:{}@{}:{}/test'.format(
         settings.POSTGRES_USER, settings.POSTGRES_PASSWORD, 
         settings.POSTGRES_HOST,settings.POSTGRES_PORT
         )
 
-engine = create_engine(DATABASE_URL)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    engine = create_engine(DATABASE_URL)
 
-def override_get_db():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+
+@pytest.fixture(scope="function")
+def override_get_db(engine: Engine):
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = Session(bind=connection)
+
+    connection.begin_nested()
+
+    listens_for(session, "after_transaction_end")
+    def restart_savepoint(session=session, transaction=transaction):
+        if transaction.nested and not transaction._parent.nested:
+            session.begin_nested()
+
+    yield session
+
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+client = TestClient(app=app)
+"""@pytest.fixture(scope="function")
+def client():
+    with Client(app=app, base_url='127.0.0.1:8000/') as sync_client:
+        yield sync_client"""
+
+"""@pytest.fixture(scope="function")
+def override_get_db(engine: Engine):
     connection = engine.connect()
 
       # begin a non-ORM transaction
@@ -31,7 +67,4 @@ def override_get_db():
     yield db
 
     db.rollback()
-    connection.close()
-
-
-client = TestClient(app)
+    connection.close()"""
--- tests/test_users/test_routers.py
+++ tests/test_users/test_routers.py
@@ -3,14 +3,12 @@ from tests.test_users.fixtures import client
 
 def test_create_user(client=client):
     response = client.post("api/users/register", json={
-        "first_name": "string_aaa",
-        "last_name": "string_aaa",
-        "email": "string_aaaa",
-        "birth_date": "2022-09-04",
-        "username": "string_aaa",
+        "first_name": "bbdw",
+        "last_name": "ccdw",
+        "email": "xxdw",
+        "birth_date": "2021-02-03",
+        "username": "bcxbcxdw",
         "password": "stringst",
         "password_repeat": "stringst"}
     )
-    response2 = client.get("api/users")
-    print(response2.json())
     assert response.status_code == 201
