4245f0ca5f261f9c06aa926c8f896dc57dddc969
michal999 <71217112+mmyszak999@users.noreply.github.com>
2023-03-21 19:18:55 +0100

    fix product update service

--- src/apps/products/services/product_services.py
+++ src/apps/products/services/product_services.py
@@ -57,11 +57,6 @@ def update_single_product(session: Session, product: ProductInputSchema, product
 
     rows = [{"product_id": product_id, "category_id": category.get('id')} for category in product_data['categories']]
     insert(association_table).values(rows)
-    categories = [cat.get('id') for cat in product_data['categories']]
-    w = session.execute(select(Category).filter(Category.id.in_(categories))).all()
-    prod = session.execute(select(Product).filter(Product.id==product_id)).one()
-    for item in w:
-        prod[0].categories.append(item[0]) if item[0].id not in categories else print("next")
     product_data.pop('categories')
     statement = update(Product).filter(Product.id==product_id).values(**product_data)
 
