293aeef1a9ee645bcd347bedc2b4905f1f99cecb
michal999 <71217112+mmyszak999@users.noreply.github.com>
2023-03-26 10:58:20 +0200

    improve product_services + exceptions and remove redundant code

--- src/apps/products/exceptions.py
+++ src/apps/products/exceptions.py
@@ -11,3 +11,15 @@ category_already_exists_exception=HTTPException(
 category_name_is_occupied_exception=HTTPException(
     status_code=status.HTTP_400_BAD_REQUEST, detail="Category name is occupied."
     )
+
+product_does_not_exist_exception=HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND, detail="Product doesn't exist.", 
+    )
+
+product_already_exists_exception=HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST, detail="Product already exists."
+    )
+
+product_name_is_occupied_exception=HTTPException(
+    status_code=status.HTTP_400_BAD_REQUEST, detail="Product name is occupied."
+    )
--- src/apps/products/services/product_services.py
+++ src/apps/products/services/product_services.py
@@ -10,6 +10,11 @@ from src.apps.products.schemas import (
 )
 from src.apps.products.models import Category, Product, association_table
 from src.apps.products.services.category_services import get_single_category
+from src.apps.products.exceptions import (
+    product_already_exists_exception,
+    product_does_not_exist_exception,
+    product_name_is_occupied_exception
+)
 
 
 def create_product(session: Session, product: ProductInputSchema) -> ProductOutputSchema:
@@ -17,7 +22,7 @@ def create_product(session: Session, product: ProductInputSchema) -> ProductOutp
 
     name_check = session.execute(select(Product).filter(Product.name == product_data["name"]))
     if name_check:
-        pass
+        raise product_name_is_occupied_exception
     
     categories = product_data.pop('categories')
     product_data['categories'] = [
@@ -30,27 +35,26 @@ def create_product(session: Session, product: ProductInputSchema) -> ProductOutp
     return ProductOutputSchema.from_orm(new_product)
 
 def get_single_product(session: Session, product_id: int) -> ProductOutputSchema:
-    statement = select(Product).filter(Product.id == product_id).limit(1)
-    if session.scalar(statement) is None:
-        pass
+    product_object = session.execute(select(Product).filter(Product.id==product_id)).scalar()
+    if not product_object:
+        raise product_does_not_exist_exception
 
     instance = session.execute(statement).scalar()
     return ProductOutputSchema.from_orm(instance)
 
 def get_all_products(session: Session) -> list[ProductOutputSchema]:
-    statement = select(Product)
-    instances = session.execute(statement).scalars()
+    instances = session.execute(select(Product)).scalars()
 
     return [ProductOutputSchema.from_orm(instance) for instance in instances]
 
 def update_single_product(session: Session, product: ProductInputSchema, product_id: int) -> ProductOutputSchema:
     product_object = session.execute(select(Product).filter(Product.id==product_id)).scalar()
-    if product_object is None:
-        pass
+    if not product_object:
+        raise product_does_not_exist_exception
     
-    name_check = session.execute(select(Product).filter(Product.name == product.name))
-    if name_check.first():
-        pass
+    product_name_check = session.execute(select(Product).filter(Product.name == product.name))
+    if product_name_check.first():
+        raise product_name_is_occupied_exception
     
     product_data = product.dict()
     incoming_categories = set(category['id'] for category in product_data['categories'])
@@ -76,8 +80,8 @@ def update_single_product(session: Session, product: ProductInputSchema, product
     return get_single_product(session, product_id=product_id)
 
 def delete_single_product(session: Session, product_id: int):
-    if_exists = select(Product).filter(Product.id == product_id)
-    if session.scalar(if_exists) is None:
+    product_object = session.execute(select(Product).filter(Product.id==product_id)).scalar()
+    if not product_object:
         pass
 
     statement = delete(Product).filter(Product.id == product_id)
