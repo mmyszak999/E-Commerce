fbc57a9e6aa21c18678c58e7819c9daad0620907
michal999 <71217112+mmyszak999@users.noreply.github.com>
2023-06-06 14:35:53 +0200

    add order tests data

--- tests/test_orders/conftest.py
+++ tests/test_orders/conftest.py
@@ -3,8 +3,46 @@ import copy
 from sqlalchemy.orm import Session
 
 from src.apps.orders.schemas import OrderInputSchema, OrderOutputSchema
-from src.apps.products.services.category_services import create_category, delete_all_categories, get_single_category
-from src.apps.products.services.product_services import create_product, delete_all_products
+from src.apps.orders.services import create_order, delete_all_orders, get_single_order
 from tests.test_users.conftest import get_token_header, db_users
 
 from src.core.pagination.models import PageParams
+
+LIST_OF_ORDER_INPUT_SCHEMAS = [
+    OrderInputSchema(),
+    OrderInputSchema(),
+    OrderInputSchema()
+]
+
+CREATE_ORDER_DATA = {
+}
+
+UPDATE_ORDER_DATA = {
+}
+
+EXISTING_ORDER_DATA = LIST_OF_ORDER_INPUT_SCHEMAS[0]
+
+
+@pytest.fixture
+def db_orders(sync_session: Session, db_products, db_users):
+    delete_all_orders(sync_session)
+    for order in LIST_OF_ORDER_INPUT_SCHEMAS:
+        order.product_ids = [db_products[LIST_OF_ORDER_INPUT_SCHEMAS.index(order)].id]
+        order.user_id = [db_users[LIST_OF_ORDER_INPUT_SCHEMAS.index(order)].id]
+    
+    return [create_order(sync_session, order) for order in LIST_OF_ORDER_INPUT_SCHEMAS]
+    
+@pytest.fixture
+def post_order(db_products, db_users) -> dict[str, str]:
+    CREATE_PRODUCT_DATA['product_ids'] = [db_products[1].id, db_products[2].id]
+    CREATE_PRODUCT_DATA['user_id'] = db_users[0].id
+    return CREATE_ORDER_DATA
+
+@pytest.fixture
+def update_order(db_products) -> dict[str, str]:
+    UPDATE_ORDER_DATA['product_ids'] = [db_products[0].id]
+    return UPDATE_ORDER_DATA
+
+@pytest.fixture
+def create_existing_order_data() -> OrderInputSchema:
+    return EXISTING_PRODUCT_DATA
