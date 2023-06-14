123edd4c7f69f4a5a739f2ff1213a3bf452ac6eb
michal999 <71217112+mmyszak999@users.noreply.github.com>
2023-06-14 13:11:44 +0200

    refactor user schemas tests

--- tests/test_users/test_schemas.py
+++ tests/test_users/test_schemas.py
@@ -8,14 +8,15 @@ from pydantic.error_wrappers import ValidationError
 from src.apps.user.schemas import UserRegisterSchema
 from src.core.factories import UserFactory
 
-def test_passwords_are_not_identical():
+@pytest.mark.parametrize('incorrect_password', 'passwordX')
+def test_passwords_are_not_identical(incorrect_password):
     with pytest.raises(ValidationError):
-        user_data = UserFactory.build(password="password", password_repeat="passwordX")
+        user_data = UserFactory.build(password="password", password_repeat=incorrect_password)
         UserRegisterSchema(**user_data)
-
-def test_date_is_from_future():
+        
+@pytest.mark.parametrize('future_date', [datetime.now() + timedelta(days=1)])
+def test_date_is_from_future(future_date):
     with pytest.raises(ValidationError):
-        future_date = datetime.now()+timedelta(days=1)
         user_data = UserFactory.build(birth_date=future_date, password="password", password_repeat="password")
         UserRegisterSchema(**user_data)
 
