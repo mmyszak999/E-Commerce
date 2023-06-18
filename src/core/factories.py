af89e2756741c874a38b085943bdf646913fdac9
michal999 <71217112+mmyszak999@users.noreply.github.com>
2023-06-18 12:27:21 +0200

    add order factory

--- src/core/factories.py
+++ src/core/factories.py
@@ -1,7 +1,13 @@
 from pydantic_factories import ModelFactory
 
 from src.apps.user.schemas import UserRegisterSchema
+from src.apps.orders.schemas import OrderInputSchema
 
 
 class UserFactory(ModelFactory):
     __model__ = UserRegisterSchema
+
+
+class OrderFactory(ModelFactory):
+    __model__ = OrderInputSchema
+    
