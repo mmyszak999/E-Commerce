ba9667c1b16881a14df8300949af14dcb0bbaf88
michal999 <71217112+mmyszak999@users.noreply.github.com>
2023-07-18 17:58:26 +0200

    enhance admin routers tests

--- tests/test_admin/test_routers.py
+++ tests/test_admin/test_routers.py
@@ -16,30 +16,10 @@ def test_superuser_can_get_all_superusers(
     assert response.json()["total"] == 1
 
 
-def test_superuser_can_grant_superuser_status(
-    sync_client: TestClient, superuser_auth_headers: dict[str, str], db_user: UserOutputSchema
-):
-    response = sync_client.patch(f"admin/grant-superuser-permissions/user/{db_user.id}", headers=superuser_auth_headers)
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json()["message"] == "Superuser status has been granted successfully"
-
-
-def test_superuser_can_revoke_superuser_status(
-    sync_client: TestClient, superuser_auth_headers: dict[str, str], db_user: UserOutputSchema
-):
-    response = sync_client.patch(f"admin/grant-superuser-permissions/user/{db_user.id}", headers=superuser_auth_headers)
-    assert response.status_code == status.HTTP_200_OK
-    
-    response = sync_client.patch(f"admin/revoke-superuser-permissions/user/{db_user.id}", headers=superuser_auth_headers)
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json()["message"] == "Superuser status has been revoked successfully"
-
-
 def test_new_superuser_can_get_all_superusers(
     sync_client: TestClient, superuser_auth_headers: dict[str, str], db_user: UserOutputSchema, auth_headers: dict[str, str]
 ):
-    response = sync_client.patch(f"admin/grant-superuser-permissions/user/{db_user.id}", headers=superuser_auth_headers)
-    assert response.status_code == status.HTTP_200_OK
+    sync_client.patch(f"admin/grant-superuser-permissions/user/{db_user.id}", headers=superuser_auth_headers)
     
     response = sync_client.get("admin/superusers", headers=auth_headers)
     
@@ -53,6 +33,42 @@ def test_non_superuser_cannot_get_all_superusers(
     response = sync_client.get("admin/superusers", headers=auth_headers)
     
     assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_superuser_can_grant_superuser_status(
+    sync_client: TestClient, superuser_auth_headers: dict[str, str], db_user: UserOutputSchema
+):
+    response = sync_client.patch(f"admin/grant-superuser-permissions/user/{db_user.id}", headers=superuser_auth_headers)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["message"] == "Superuser status has been granted successfully"
+    
+    
+def test_non_superuser_cannot_grant_superuser_status(
+    sync_client: TestClient, auth_headers: dict[str, str]
+):
+    response = sync_client.patch("admin/grant-superuser-permissions/user/2", headers=auth_headers)
+    
+    assert response.status_code == status.HTTP_403_FORBIDDEN  
+
+
+def test_superuser_can_revoke_superuser_status(
+    sync_client: TestClient, superuser_auth_headers: dict[str, str], db_user: UserOutputSchema
+):
+    response = sync_client.patch(f"admin/grant-superuser-permissions/user/{db_user.id}", headers=superuser_auth_headers)
+    assert response.status_code == status.HTTP_200_OK
+    
+    response = sync_client.patch(f"admin/revoke-superuser-permissions/user/{db_user.id}", headers=superuser_auth_headers)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["message"] == "Superuser status has been revoked successfully"
+
+
+def test_non_superuser_cannot_revoke_superuser_status(
+    sync_client: TestClient, auth_headers: dict[str, str]
+):
+    response = sync_client.patch("admin/revoke-superuser-permissions/user/2", headers=auth_headers)
+    
+    assert response.status_code == status.HTTP_403_FORBIDDEN  
+
 
 
 
